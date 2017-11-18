da8a84e3b3e063c9dde814735da6aa82ba4f8313
hiriorima <g2117001@fun.ac.jp>
2017-11-18 22:37:15 +0900

    Refactor LoginViewController by divide UITextFieldDelegate

--- KidsFab/Views/LoginViewController.swift
+++ KidsFab/Views/LoginViewController.swift
@@ -29,8 +29,7 @@ func >= <T: Comparable>(lhs: T?, rhs: T?) -> Bool {
     }
 }
 
-class LoginViewController: UIViewController,
-UITextFieldDelegate, UIScrollViewDelegate {
+class LoginViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var IDInputField: UITextField!
     @IBOutlet var PWInputField: UITextField!
@@ -46,8 +45,8 @@ UITextFieldDelegate, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         sc.frame = self.view.frame
-        IDInputField.delegate = self
-        PWInputField.delegate = self
+
+        setTextFieldDelegate()
         sc.delegate = self
         
         IDInputField.placeholder = "IDを入力してください(英数字3~10字)"
@@ -60,44 +59,6 @@ UITextFieldDelegate, UIScrollViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    /*
-     編集開始時の処理
-     *パスワード入力方式設定
-     *if:テキストフィールドをタップ
-     テキストフィールド初期化
-     */
-    @IBAction func TextFieldEditingDidBegin(_ sender: UITextField) {
-        txtActiveField = sender
-        if sender == PWInputField {
-            sender.isSecureTextEntry = true
-        }
-    }
-    
-    /*
-     テキストが編集された際に呼ばれる.
-     */
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        var maxLength: Int = 0
-        
-        // 文字数最大を決める.
-        if textField == IDInputField {
-            maxLength = 11
-        } else if textField == PWInputField {
-            maxLength = 9
-        }
-        
-        // 入力済みの文字と入力された文字を合わせて取得.
-        let str = textField.text! + string
-        
-        // 文字数がmaxLength以下ならtrueを返す.
-        if str.characters.count < maxLength {
-            return true
-        }
-        
-        return false
-    }
 
     /*
      キーボード以外をタップするとキーボードを閉じる
@@ -107,44 +68,18 @@ UITextFieldDelegate, UIScrollViewDelegate {
     }
     
     /*
-     Returnをタップするとキーボードを閉じる
-     */
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    /*
      キーボード表示時にテキストフィールドと重なっているか調べる
      重なっていたらスクロールする
      */
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(LoginViewController.handleKeyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(LoginViewController.handleKeyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-    }
-    
-    func handleKeyboardWillShowNotification(_ notification: Notification) {
-        
-        let userInfo = notification.userInfo!
-        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-        let myBoundSize: CGSize = UIScreen.main.bounds.size
-        let txtLimit = txtActiveField.frame.origin.y + txtActiveField.frame.height + 8.0
-        let kbdLimit = myBoundSize.height - (keyboardScreenEndFrame?.size.height)!
-        
-        if txtLimit >= kbdLimit {
-            sc.contentOffset.y = txtLimit - kbdLimit
-        }
-    }
-    
-    func handleKeyboardWillHideNotification(_ notification: Notification) {
-        sc.contentOffset.y = 0
+        setKeyboardNotification()
     }
     
     @IBOutlet var LoginButton: SpringButton!
     var LoginFlag = (0, 0)
+    
     @IBAction func TapLoginButton(_ sender: AnyObject) {
         let String_ID = IDInputField.text
         let String_PW = PWInputField.text
@@ -210,15 +145,81 @@ UITextFieldDelegate, UIScrollViewDelegate {
         // Viewの移動する.
         self.present(HomeScreenViewController, animated: true, completion: nil)
     }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+
+    func setTextFieldDelegate() {
+        IDInputField.delegate = self
+        PWInputField.delegate = self
+    }
     
     /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
+     Returnをタップするとキーボードを閉じる
      */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
+    func handleKeyboardWillShowNotification(_ notification: Notification) {
+        
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        let myBoundSize: CGSize = UIScreen.main.bounds.size
+        let txtLimit = txtActiveField.frame.origin.y + txtActiveField.frame.height + 8.0
+        let kbdLimit = myBoundSize.height - (keyboardScreenEndFrame?.size.height)!
+        
+        if txtLimit >= kbdLimit {
+            sc.contentOffset.y = txtLimit - kbdLimit
+        }
+    }
+    
+    func handleKeyboardWillHideNotification(_ notification: Notification) {
+        sc.contentOffset.y = 0
+    }
+    
+    func setKeyboardNotification() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(LoginViewController.handleKeyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(LoginViewController.handleKeyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    /*
+     編集開始時の処理
+     *パスワード入力方式設定
+     *if:テキストフィールドをタップ
+     テキストフィールド初期化
+     */
+    @IBAction func TextFieldEditingDidBegin(_ sender: UITextField) {
+        txtActiveField = sender
+        if sender == PWInputField {
+            sender.isSecureTextEntry = true
+        }
+    }
+    
+    /*
+     テキストが編集された際に呼ばれる.
+     */
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        var maxLength: Int = 0
+        
+        // 文字数最大を決める.
+        if textField == IDInputField {
+            maxLength = 11
+        } else if textField == PWInputField {
+            maxLength = 9
+        }
+        
+        // 入力済みの文字と入力された文字を合わせて取得.
+        let str = textField.text! + string
+        
+        // 文字数がmaxLength以下ならtrueを返す.
+        if str.characters.count < maxLength {
+            return true
+        }
+        
+        return false
+    }
 }
